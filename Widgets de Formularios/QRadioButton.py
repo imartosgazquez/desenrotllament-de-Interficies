cdff48569158750b3fb539d8403b0d88867084f7
Iván Martos <i.martosgazquez@edu.gva.es>
2023-10-19 09:33:07 +0100

    Actualización 19 Octubre2

--- Widgets de Formularios/QRadioButton.py
+++ Widgets de Formularios/QRadioButton.py
@@ -1,38 +1,27 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QRadioButton,QHBoxLayout,QWidget
-from PySide6.QtCore import QSize, Qt
-import sys
-
+from PySide6.QtWidgets import QApplication, QMainWindow, QRadioButton
 
 class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        layout = QHBoxLayout()
-        # creamos un botón radial y lo establecemos de widget central
-        radial1 = QRadioButton("Botón radial 1")
-        radial2 = QRadioButton("Botón radial 2")
-        layout.addWidget(radial1)
-        layout.addWidget(radial2)
-        # creamos un dummy widget para hacer de contenedor
-        widget = QWidget()
-
-        # le asignamos el layout
-        widget.setLayout(layout)
-        self.setCentralWidget(widget)
-
-        # señal para detectar cambios en el botón
-        radial1.toggled.connect(self.estado_cambiado)
-
-        # Podemos activarla por defecto
-        radial1.setChecked(True)
-
-        # consultamos el valor actual
-        print("¿Activada?", radial1.isChecked())
-
-    def estado_cambiado(self, estado):
-        if estado:
-            print("Radial marcado")
-        else:
-            print("Radial desmarcado")
+    def __init__(self): 
+        super().__init__() 
+ 
+        # creamos un botón radial y lo establecemos de widget central 
+        radial = QRadioButton("Botón radial") 
+        self.setCentralWidget(radial) 
+ 
+        # señal para detectar cambios en el botón 
+        radial.toggled.connect(self.estado_cambiado) 
+ 
+        # Podemos activarla por defecto 
+        radial.setChecked(True) 
+ 
+        # consultamos el valor actual 
+        print("¿Activada?", radial.isChecked()) 
+ 
+    def estado_cambiado(self, estado): 
+        if estado: 
+            print("Radial marcado") 
+        else: 
+            print("Radial desmarcado") 
 
 if __name__ == "__main__":
     app = QApplication([])
